ff760323d2a67a7b0b536a70dafb5849bac9b3da
erkamguresen <erkamguresen@gmail.com>
2022-03-08 21:15:53 +0100

    app user repository

--- src/main/java/app/mymemo/backend/appuser/AppUserRepository.java
+++ src/main/java/app/mymemo/backend/appuser/AppUserRepository.java
@@ -1,4 +1,28 @@
 package app.mymemo.backend.appuser;
 
-public interface AppUserRepository {
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface AppUserRepository extends CrudRepository<AppUser, String> {
+
+    /**
+     * Retrieves an entity by its username.
+     * @param username must not be null.
+     * @return an Optional describing the AppUser with the given username,
+     * or an empty Optional if the username is not found in the database
+     */
+    @Query("{ 'username' : ?0 }")
+    Optional<AppUser> findUserByUsername(String username);
+
+    /**
+     * Retrieves an entity by its email.
+     * @param email must not be null.
+     * @return an Optional describing the AppUser with the given username,
+     * or an empty Optional if the username is not found in the database
+     */
+    @Query("{ 'email' : ?0 }")
+    Optional<AppUser> findUserByEmail(String email);
+
 }
