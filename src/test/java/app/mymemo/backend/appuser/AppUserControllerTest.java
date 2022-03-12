70a95d1d373041d68cbc3f34a8e5ac91c6a81208
erkamguresen <erkamguresen@gmail.com>
2022-03-12 09:54:45 +0100

    basic user integration tests with mock

--- src/test/java/app/mymemo/backend/appuser/AppUserControllerTest.java
+++ src/test/java/app/mymemo/backend/appuser/AppUserControllerTest.java
@@ -5,16 +5,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.util.*;
 
 
@@ -23,6 +23,10 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -37,91 +41,97 @@ class AppUserControllerTest {
     private AppUserRepository userRepository;
     @Autowired
     private ObjectMapper objectMapper;
+
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private AppUserService appUserServiceMock;
+
 
     @BeforeEach
     void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void itShouldGetUsers() throws Exception {
+        /*setup mock*/
+        List<AppUser> users = new ArrayList<>();
+
         String email = "jhondoe@gmail.com";
         List<AppUserRole> roles = new ArrayList<>();
         roles.add(AppUserRole.APP_USER_ROLE);
-        AppUser user = new AppUser(
+        AppUser firstUser = new AppUser(
                 "John",
                 "Doe",
                 email,
                 "password",
                 roles
         );
-        userRepository.save(user);
+        users.add(firstUser);
 
         email = "janedoe@gmail.com";
         roles = new ArrayList<>();
         roles.add(AppUserRole.APP_ADMIN_ROLE);
 
-        user = new AppUser(
+        AppUser secondUser = new AppUser(
                 "Jane",
                 "Doe",
                 email,
                 "password",
                 roles
         );
-        userRepository.save(user);
-    }
+        users.add(secondUser);
 
-    @AfterEach
-    void tearDown() {
-        userRepository.deleteAll();
-    }
+        when(appUserServiceMock.findAllUsers()).thenReturn(users);
 
-    @Test
-    void getUsers() throws JsonProcessingException {
-        String url = "http://localhost:" + port + "/api/v1/users";
+        mockMvc.perform(get("/api/v1/users")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].firstName").value("John"))
+                .andExpect(jsonPath("$[0].lastName").value("Doe"))
+                .andExpect(jsonPath("$[0].email").value("jhondoe@gmail.com"))
+                .andExpect(jsonPath("$[0].password").value("password"))
+                .andExpect(jsonPath("$[0].roles[0]").value("APP_USER_ROLE"))
+                .andExpect(jsonPath("$[0].enabled").value(false))
+                .andExpect(jsonPath("$[0].credentialsNonExpired").value(true))
+                .andExpect(jsonPath("$[0].accountNonExpired").value(true))
+                .andExpect(jsonPath("$[0].accountLocked").value(false))
+                .andExpect(jsonPath("$[0].accountNonLocked").value(true))
+                .andExpect(jsonPath("$[0].accountEnabled").value(false))
+                .andExpect(jsonPath("$[0].authorities[0].authority").value("APP_USER_ROLE"))
+                .andExpect(jsonPath("$[0].username").value("jhondoe@gmail.com"))
 
-        UriComponents builder = UriComponentsBuilder.fromHttpUrl(url)
-                .build();
-        HttpEntity<String> requestEntity = new HttpEntity<>(null, null);
-        ResponseEntity<String> response = testRestTemplate.exchange(builder.toString(), HttpMethod.GET, requestEntity,
-                String.class);
-
-        System.out.println(response.getBody());
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-
-        String[] userStrings = response.getBody()
-                .replace("},{", "} split here {")
-                .split(" split here ");
-
-        System.out.println(userStrings[0]);
-        assertThat(userStrings[0])
-                .contains("\"firstName\":\"John\",\"lastName\":\"Doe\",\"email\":\"jhondoe@gmail.com\",\"password\":\"password\",\"roles\":[\"APP_USER_ROLE\"]")
-                .contains("\"username\":\"jhondoe@gmail.com\"")
-                .contains("\"authorities\":[{\"authority\":\"APP_USER_ROLE\"}]")
-                .contains("\"enabled\":false")
-                .contains("\"credentialsNonExpired\":true")
-                .contains("\"accountNonExpired\":true")
-                .contains("\"accountEnabled\":false")
-                .contains("\"accountLocked\":false")
-                .contains("\"accountNonLocked\":true");
-
-        assertThat(userStrings[1])
-                .contains("\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"email\":\"janedoe@gmail.com\",\"password\":\"password\",\"roles\":[\"APP_ADMIN_ROLE\"]")
-                .contains("\"username\":\"janedoe@gmail.com\"")
-                .contains("\"authorities\":[{\"authority\":\"APP_ADMIN_ROLE\"}]")
-                .contains("\"enabled\":false")
-                .contains("\"credentialsNonExpired\":true")
-                .contains("\"accountNonExpired\":true")
-                .contains("\"accountEnabled\":false")
-                .contains("\"accountLocked\":false")
-                .contains("\"accountNonLocked\":true");
+                .andExpect(jsonPath("$[1].firstName").value("Jane"))
+                .andExpect(jsonPath("$[1].lastName").value("Doe"))
+                .andExpect(jsonPath("$[1].email").value("janedoe@gmail.com"))
+                .andExpect(jsonPath("$[1].password").value("password"))
+                .andExpect(jsonPath("$[1].roles[0]").value("APP_ADMIN_ROLE"))
+                .andExpect(jsonPath("$[1].enabled").value(false))
+                .andExpect(jsonPath("$[1].credentialsNonExpired").value(true))
+                .andExpect(jsonPath("$[1].accountNonExpired").value(true))
+                .andExpect(jsonPath("$[1].accountLocked").value(false))
+                .andExpect(jsonPath("$[1].accountNonLocked").value(true))
+                .andExpect(jsonPath("$[1].accountEnabled").value(false))
+                .andExpect(jsonPath("$[1].authorities[0].authority").value("APP_ADMIN_ROLE"))
+                .andExpect(jsonPath("$[1].username").value("janedoe@gmail.com"));
 
     }
 
     @Test
-    void getUser() {
+    void itShouldGetUser()  {
+
     }
 
     @Test
     void updateUser() {
+
     }
 
     @Test
